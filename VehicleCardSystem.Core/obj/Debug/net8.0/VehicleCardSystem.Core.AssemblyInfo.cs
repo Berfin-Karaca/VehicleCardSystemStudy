0213beca68d230d98e5d2ddbd732e8e46c9e6e43
Berfin-Karaca <karacaberfin.bkbk67@gmail.com>
2025-07-31 03:18:04 +0300

    Readme detaylı dolduruldu.

--- VehicleCardSystem.Core/obj/Debug/net8.0/VehicleCardSystem.Core.AssemblyInfo.cs
+++ VehicleCardSystem.Core/obj/Debug/net8.0/VehicleCardSystem.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleCardSystem.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f41cece1d86f8e86d948b5b728b2533e8144b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c8322006daa60629dd3f04f88c3686bf05a181")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleCardSystem.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleCardSystem.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
